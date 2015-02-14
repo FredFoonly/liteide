93df1bbbd961a2f4357d1220aa03bf38750722f7
FredFoonly <kmikep@live.com>
2015-02-14 11:32:25 -0600

    added a deploy_openbsd.sh script to copy the executables and assorted other stuff to the /usr/local directory.

---
+++ build/deploy_openbsd.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export BUILD_ROOT=$PWD
+
+if [ -z $LITEIDE_DEPLOYMENT_ROOT ]; then
+	export LITEIDE_DEPLOYMENT_ROOT=$PWD/liteide
+fi
+
+echo deploy liteide
+echo LITEIDE_DEPLOYMENT_ROOT=$LITEIDE_DEPLOYMENT_ROOT 
+echo .
+
+echo Copying liteide to /usr/local
+cp -rf $LITEIDE_DEPLOYMENT_ROOT/local /usr/local
